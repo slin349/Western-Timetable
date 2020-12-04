71d69113827345244183f7d1a91e42ea9ead63fb
Steven <stevenlin159@gmail.com>
2020-12-03 21:49:29 -0500

    expanding div bug working again

--- Frontend/src/app/schedule/schedule.component.ts
+++ Frontend/src/app/schedule/schedule.component.ts
@@ -11,8 +11,8 @@ export class ScheduleComponent implements OnInit {
   deleteschedulename = "";
   schedules = [];
   courseinfo = [];
-  temparray = [];
   show = -1;
+  temparray = [];
 
   constructor() { }
 
@@ -59,19 +59,18 @@ export class ScheduleComponent implements OnInit {
             res.json()
             .then (data => {
                 data.forEach(element => {
-
-                    //stop after 10 schedules
-                    if (count > 10){
-                      return;
-                    }
                     fetch (`http://localhost:3000/timetable/schedule/${element.ScheduleName}`)
                     .then (res => {
                         res.json()
                         .then (data => {
-
                           //if visibility is true
                           if (data[4] == true){
                             count ++;
+                            //stop after 10 schedules
+                            if (count > 10){
+                              return;
+                            }
+
                             this.temparray.push(data);          
                             this.temparray.sort((a,b) => {
                             return b[6] - a[6];
@@ -97,20 +96,14 @@ export class ScheduleComponent implements OnInit {
     //clear array
     this.courseinfo = [];
 
-    if (this.show === i){
-      this.show = -1;
-    }
-    else {
-      this.show = i;
-    }
-
     //if no courses
-    if (this.schedules[i].length < 7){
+    if (this.schedules[i].length < 8){
       return;
     }
 
     //if greater 7 means there is courses
     for (var j = 7; j<this.schedules[i].length; j++ ){
+      
       const subjectname = this.schedules[i][j].SubjectCode;
       const coursecode = this.schedules[i][j].CourseCode;
 
@@ -120,8 +113,13 @@ export class ScheduleComponent implements OnInit {
       .then (data => {
         this.courseinfo.push(data);
       })
-
+    }
+    if (this.show === i){
+      this.show = -1;
+    }
+    else {
+      this.show = i;
     }
   }
-  
+
 }
